473fc3f62fe67cc8f94e5ca9d680f32e7f2d8e05
Universe1122 <whdals7979@naver.com>
2024-03-22 21:45:44 +0900

    수정

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ var io = require('socket.io')(server);
 var chokidar = require('chokidar');
 var JsonRefs = require('json-refs');
 var yaml = require('js-yaml');
+var client_server_info = '';
+
+app.set('view engine', 'ejs');
 
 function dictToString(dict) {
   var res = [];
@@ -21,8 +24,17 @@ function dictToString(dict) {
   return res.join('\n');
 }
 
-function bundle(swaggerFile) {
+function bundle(swaggerFile, server) {
   var root = yaml.safeLoad(fs.readFileSync(swaggerFile, 'utf8'));
+  var data;
+
+  for(const info of root) {
+    if(info["host"] == server){
+      data = info["openapi"];
+      break;
+    }
+  }
+
   var options = {
     filter : ['relative', 'remote'],
     resolveCirculars: true,
@@ -34,7 +46,7 @@ function bundle(swaggerFile) {
     }
   };
   JsonRefs.clearCache();
-  return JsonRefs.resolveRefs(root, options).then(function (results) {
+  return JsonRefs.resolveRefs(data, options).then(function (results) {
     var resErrors = {};
     for (const [k,v] of Object.entries(results.refs)) {
       if ('missing' in v && v.missing === true && (v.type == 'relative' || v.type === 'remote'))
@@ -56,32 +68,40 @@ function bundle(swaggerFile) {
 }
 
 function start(swaggerFile, targetDir, port, hostname, openBrowser, swaggerUIOptions, watchIgnore) {
-  app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/index.html");
-  });
-
-  app.use(express.static(swaggerEditorDist));
+  // app.use(express.static(swaggerEditorDist));
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+  app.use(express.static(path.join(__dirname, 'static')));
+
+  app.get('/', function(req, res) {
+    const server_list = getServerList(swaggerFile);
+    return res.render(__dirname + "/views/index", {server_list: server_list});
+    
+    // const server_list = getServerList(swaggerFile);
+    // console.log(server_list);
+    // return res.render(__dirname + "/views/index", {server_list : server_list});
+  });
 
   io.on('connection', function(socket) {
     socket.on('swaggerReady', function (data) {
-      bundle(swaggerFile).then(function (bundled) {
+      client_server_info = data;
+      bundle(swaggerFile, data).then(function (bundled) {
         socket.emit('updateSpec', JSON.stringify(bundled));
       }, function (err) {
         socket.emit('showError', err);
       });
     });
     socket.once('uiReady', function(data) {
+      console.log("uiReady");
       socket.emit('swaggerOptions', swaggerUIOptions);
     });
   });
 
   chokidar.watch(targetDir, {ignored: watchIgnore}).on('change', function(eventType, name) {
-    bundle(swaggerFile).then(function (bundled) {
+    bundle(swaggerFile, client_server_info).then(function (bundled) {
       console.log("File changed. Sent updated spec to the browser.");
       var bundleString = JSON.stringify(bundled, null, 2);
       io.sockets.emit('updateSpec', bundleString);
@@ -95,6 +115,20 @@ function start(swaggerFile, targetDir, port, hostname, openBrowser, swaggerUIOpt
     console.log(`Listening on ${serverUrl}`);
     if (openBrowser) open(serverUrl);
   });
+
+}
+
+function getServerList(swaggerFile) {
+  var root = yaml.safeLoad(fs.readFileSync(swaggerFile, 'utf8'));
+  const server_list = [];
+
+  for(const info of root) {
+    server_list.push(info["host"]);
+  }
+
+  const uniqueItems = new Set(server_list);
+    // 배열로 변환하여 반환
+  return Array.from(uniqueItems);
 }
 
 function build (swaggerFile, targetDir, bundleTo) {
@@ -118,3 +152,6 @@ module.exports = {
   start: start,
   build: build
 };
+
+
+start("C:\\Users\\whdal\\Downloads\\test2.json", "C:\\Users\\whdal\\Downloads", 8000, "127.0.0.1", true, {}, undefined)
